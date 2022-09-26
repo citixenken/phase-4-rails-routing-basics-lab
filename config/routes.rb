bcc042abac7e58a213c10b423b76eee3a7e64b18
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-26 21:32:24 +0300

    commit tree 0a83df817dbfc1aa948b31bd4ced8b749f4b69c6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # index route
+  get '/students', to: 'students#index'
+
+  # grades route
+  get '/students/grades', to: 'students#grades'
+
+  #highest grade route
+  get '/students/highest-grade', to: 'students#highest_grade'
 end
+
+
